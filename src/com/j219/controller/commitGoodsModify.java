89d6997cc73ca7fb8d2cd73df9f53a6798021563
AtlanticYu <869631951@qq.com>
2018-07-02 21:46:50 +0800

    000

--- src/com/j219/controller/commitGoodsModify.java
+++ src/com/j219/controller/commitGoodsModify.java
@@ -1,6 +1,7 @@
 package com.j219.controller;
 
 import javax.servlet.http.HttpServletRequest;
+
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,13 +18,11 @@ public class commitGoodsModify {
 	public void test(HttpServletRequest req,HttpServletResponse res,String spec_no,Float price,Integer sku){
 		System.out.println("调用ModifyGoodsSpec");
 		if(sku > 0) {
-			modifyGoodsInfMapper.modifyGoodsInf(spec_no, price, sku);
+			modifyGoodsInfMapper.modifyGoodsInf(spec_no,price,sku);
 			System.out.println("修改执行完毕");
 		}
 		else {
 			System.out.println("修改值不被允许");
 		}
-		
 	}
-	
 }
